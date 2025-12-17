156bfb5d4e98a63c9ed8695ce624e849d3980187
Adarsh Kumar <adarsh.kumar232@lpu.in>
2025-12-18 01:37:17 +0530

    database connected

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from "express"
+import "dotenv/config"
+import cors from "cors"
+import http from "http"
+import { connect } from "http2"
+import { connectDB } from "./lib/db.js"
+
+const app=express()
+const server=http.createServer(app)
+
+//middleware
+app.use(express.json({limit: "4mb"}))
+app.use(cors())
+
+app.use("/api/status",(req, res)=>{
+    res.send("Server is live")
+})
+
+// mongodb connected
+await connectDB()
+
+
+const PORT=process.env.PORT ||5000;
+
+server.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
